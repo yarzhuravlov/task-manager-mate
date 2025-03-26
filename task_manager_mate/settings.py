97cfca0f65b6965ff142b722ab189936453fbca9
yet92 <zyzy92187@gmail.com>
2025-03-26 17:44:02 +0200

    add django-extensions and SQL commands logging in debug mode

--- task_manager_mate/settings.py
+++ task_manager_mate/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # external apps
+    "django_extensions"
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,19 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
