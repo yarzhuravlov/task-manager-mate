be71419aed4d733817f78c11bdf6d42da5b2eb73
yet92 <zyzy92187@gmail.com>
2025-03-27 16:49:54 +0200

    add TaskType & Task models. add admin setup for tasks app

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks.models import TaskType, Task
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "task_type",
+        "priority",
+        "deadline",
+        "is_completed",
+    )
+
+    search_fields = (
+        "name",
+        "task_type__name",
+        "assigners__username",
+    )
+
+    list_filter = (
+        "priority",
+        "is_completed"
+    )
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+Worker = get_user_model()
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Task(models.Model):
+    class Priority(models.IntegerChoices):
+        URGENT = 1, "urgent"
+        HIGH = 2, "high"
+        MEDIUM = 3, "medium"
+        LOW = 4, "low"
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, default="")
+    created_at = models.DateTimeField(default=timezone.now)
+    deadline = models.DateTimeField(null=True)
+    is_completed = models.BooleanField(default=False)
+    priority = models.SmallIntegerField(choices=Priority, default=Priority.LOW)
+    task_type = models.ForeignKey(TaskType, on_delete=models.PROTECT)
+    assigners = models.ManyToManyField(Worker)
+
+    class Meta:
+        default_related_name = "tasks"
+        ordering = ("is_completed", "deadline", "priority")
+
+    def __str__(self):
+        return f"{self.name} ({self.task_type})"
