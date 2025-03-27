b735f29c86feec0cecac0caa248de228508c4be2
yet92 <zyzy92187@gmail.com>
2025-03-27 15:50:57 +0200

    add Meta class & __str__ method for Worker. add __str__ method for Position. add Worker and Position to admin panel

--- workers/admin.py
+++ workers/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from workers.models import Worker, Position
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            None,
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "position",
+                    "email",
+                    "is_staff",
+                )
+            }
+        ),
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Position', {
+            'fields': [
+                'position',
+            ],
+        }),
+    )
+
+    list_display = UserAdmin.list_display + (
+        "position",
+    )
+
+    list_filter = UserAdmin.list_filter + (
+        "position",
+    )
+
+    search_fields = UserAdmin.search_fields + (
+        "position__name",
+    )
--- workers/models.py
+++ workers/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Position(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Worker(AbstractUser):
     position = models.ForeignKey(
@@ -12,3 +15,10 @@ class Worker(AbstractUser):
         related_name="workers",
         on_delete=models.PROTECT
     )
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.position.name})"
