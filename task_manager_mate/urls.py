59455200d6f78c47452cf94e69c9faa2172fca48
yet92 <zyzy92187@gmail.com>
2025-03-28 19:32:28 +0200

    add /accounts/ url with django.contrib.auth urls & template for login page

--- task_manager_mate/urls.py
+++ task_manager_mate/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("tasks/", include("tasks.urls")),
+    path("accounts/", include("django.contrib.auth.urls"))
 ] + debug_toolbar_urls()
