4e2b89fb6569c59dba18b705e78b2b069cde5c16
yet92 <zyzy92187@gmail.com>
2025-03-28 14:44:32 +0200

    add base Task ListView & /tasks/ url

--- task_manager_mate/urls.py
+++ task_manager_mate/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("tasks/", include("tasks.urls")),
 ] + debug_toolbar_urls()
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from tasks.models import Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    paginate_by = 15
