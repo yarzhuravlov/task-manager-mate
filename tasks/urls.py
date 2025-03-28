538a7a862429ea0e7ee3037795bf173376ca7f85
yet92 <zyzy92187@gmail.com>
2025-03-28 17:41:04 +0200

    fix: add 'task-list' name for /tasks/ urlpattern

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from tasks.views import TaskListView
 
 urlpatterns = [
-    path("", TaskListView.as_view()),
+    path("", TaskListView.as_view(), name="task_list"),
 ]
 
 app_name = "tasks"
