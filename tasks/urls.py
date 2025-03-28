9fa5a2992ae4709dea6551d06c010ba50a388a6f
yet92 <zyzy92187@gmail.com>
2025-03-28 17:51:36 +0200

    fix: change /tasks/ name to task-list

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from tasks.views import TaskListView
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="task_list"),
+    path("", TaskListView.as_view(), name="task-list"),
 ]
 
 app_name = "tasks"
