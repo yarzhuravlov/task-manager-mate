004ec7c299178496f2930094e5fa316f57253722
yet92 <zyzy92187@gmail.com>
2025-04-03 20:35:56 +0300

    add accounts/register & accounts/activate/<pk>/<token> urls

--- task_manager_mate/urls.py
+++ task_manager_mate/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
@@ -21,5 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("tasks/", include("tasks.urls")),
-    path("accounts/", include("django.contrib.auth.urls"))
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
 ] + debug_toolbar_urls()
