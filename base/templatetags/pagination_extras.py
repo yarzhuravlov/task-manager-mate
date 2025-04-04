c42f8cd9dd94c452a29c061cd00328b70770b6ad
yet92 <zyzy92187@gmail.com>
2025-04-04 13:38:51 +0300

    remove unused custom tags & upgrade register_pages_list custom tag

--- base/templatetags/pagination_extras.py
+++ base/templatetags/pagination_extras.py
@@ -2,43 +2,32 @@ import dataclasses
 
 from django import template
 
+
 register = template.Library()
-
-
-@register.simple_tag(takes_context=True)
-def build_previous_query_params(context):
-    if context["page_obj"].has_previous():
-        return f"page={context["page_obj"].previous_page_number()}"
-
-    return ""
-
-
-@register.simple_tag(takes_context=True)
-def build_next_query_params(context):
-    if context["page_obj"].has_next():
-        return f"page={context["page_obj"].next_page_number()}"
-
-    return ""
 
 
 @dataclasses.dataclass
 class Page:
     page_number: int
-    query_params: str
     active: bool
 
 
 @register.simple_tag(takes_context=True)
-def register_pages_list(context, pages_count=3):
+def register_pages_list(context: template.Context, pages_count: int = 3):
     current_page = context["page_obj"].number
     first = current_page - pages_count
     last = current_page + pages_count
 
+    if first <= 0:
+        last += -first + 1
+
+    if last > context["paginator"].num_pages:
+        first -= last - context["paginator"].num_pages
+
     context["page_list"] = [
         Page(
             page_number,
-            query_params=f"page={page_number}",
-            active=page_number == current_page
+            active=page_number == current_page,
         )
         for page_number in context["page_obj"].paginator.page_range
         if first <= page_number <= last
