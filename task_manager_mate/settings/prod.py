a9a747b6ae6be366941226a73e011ad70c01b37b
yet92 <zyzy92187@gmail.com>
2025-04-11 16:51:12 +0300

    add RENDER_EXTERNAL_HOSTNAME check & apply RENDER_EXTERNAL_HOSTNAME to ALLOWED_HOSTS if exists

--- task_manager_mate/settings/prod.py
+++ task_manager_mate/settings/prod.py
@@ -10,6 +10,10 @@ ALLOWED_HOSTS = [
     "localhost"
 ]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
