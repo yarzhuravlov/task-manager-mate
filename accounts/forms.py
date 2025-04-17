be5e96a1332d5442c19cb8759a367d96265c1587
yet92 <zyzy92187@gmail.com>
2025-04-17 17:31:42 +0300

    remove unnecessary fields and checks from RegistrationForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,8 +6,6 @@ User = get_user_model()
 
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text="Required")
-
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
@@ -17,9 +15,3 @@ class RegistrationForm(UserCreationForm):
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("This email is already in use.")
         return email
-
-    def clean_username(self):
-        username = self.cleaned_data.get("username")
-        if User.objects.filter(username=username).exists():
-            raise forms.ValidationError("This username is already in use.")
-        return username
